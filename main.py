2eb6da686f99aecfdc119c6922cea27eab7ac917
Jordan <jordaneddygarcia@gmail.com>
2020-04-28 22:29:27 -0500

    Updated main.py to be fully tested

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import numpy
 import cv2
-import flask
+import os
+from flask import Flask, flash, request, redirect, url_for
 
-print("Starting Program")
+UPLOAD_FOLDER = '/img'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
-print("Exiting Program")
+#initialize
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+#home page
+@app.route("/", methods = ['GET', 'POST'])
+def home():
+    if request.method=='POST':
+        return "<h1>POST</h1>"
+    if request.method=='GET':
+        return "<h1>GET</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
