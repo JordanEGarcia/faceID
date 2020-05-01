08efa7ce6124a4478ddf9702a02ac7253355dd1d
Jordan <jordaneddygarcia@gmail.com>
2020-05-01 07:29:52 -0500

    Added Cascades and imports for them

--- face_detect.py
+++ face_detect.py
@@ -1,18 +1,17 @@
 import cv2
 import numpy as np
 
-face_detector = cv2.CascadeClassifier('data/cascade/haarcascade_frontalface_default.xml')
-eye_detector  = cv2.CascadeClassifier('data/cascade/haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
+eye_cascade  = cv2.CascadeClassifier('cascades/haarcascade_eye.xml')
 
 # Take in an image
 #for now we'll do a test
-cap = cv2.imread('test/will_smith.jpg')
+gray = cv2.imread('test/will_smith.jpg', 0)
 
-
-gray = cv2.cvtColor(cap, cv2.COLOR_BGR2GRAY)
-#faces = face_detector.detectMultiScale(img, 1.3, 5)
-
-cv2.imshow("will_smith", cap)
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+for (x, y, w, h) in faces:
+    # Draw rectangle around the face
+    cv2.rectangle(gray, (x, y), (x+w, y+h), (255, 255, 255), 3)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
