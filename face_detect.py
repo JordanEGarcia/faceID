8e6d675fa361b8a2ffdbffe9e9ef88175d1842d8
Jordan <jordaneddygarcia@gmail.com>
2020-05-02 12:39:27 -0500

    Made faceID class

--- face_detect.py
+++ face_detect.py
@@ -1,25 +1,30 @@
 import cv2
 import numpy as np
 
-face_cascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
+class Face:
+    """
+    Face class allows you to create a face object and automatically crop
+    the image if it knows there is only one face. Saves Face Count as well
+    """
+    def __init__(self, fileName):
+        self.fileName = fileName
+        self.img = self.imread()
+        self.faces = self.faces()
+        self.faceCount = self.faceCount()
+        self.cropped_img = self.cropImg()
 
-# Take in an image
-#for now we'll do a test
-gray = cv2.imread('test/will_smith.jpg', 0)
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    def imread(self):
+        return cv2.imread(self.fileName, 0)
 
-# Crop a single face from an image.
-def faceID():
-    # Count how many faces
-    faceCount = faces.shape[0]
-    if faceCount==1:
-        x, y, w, h = faces[0]
-        crop_img = gray[y: y + h, x: x + w]
-        cv2.imwrite("test/face.jpg", crop_img)
-        return True
-    else:
-        return False
-faceID()
+    def faceCount(self):
+        return self.faces.shape[0]
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    def cropImg(self):
+        if self.faceCount==1:
+            x, y, w, h = self.faces[0]
+            crop_img = self.img[y: y + h, x: x + w]
+            return crop_img
+
+    def faces(self):
+        face_cascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
+        return face_cascade.detectMultiScale(self.img, 1.3, 5)
