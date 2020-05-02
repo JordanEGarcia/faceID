9241e695178d3dacc389d857815532e08260685c
Jordan <jordaneddygarcia@gmail.com>
2020-05-01 22:44:24 -0500

    Face Detect will now crop image of the face we need

--- face_detect.py
+++ face_detect.py
@@ -2,16 +2,24 @@ import cv2
 import numpy as np
 
 face_cascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
-eye_cascade  = cv2.CascadeClassifier('cascades/haarcascade_eye.xml')
 
 # Take in an image
 #for now we'll do a test
 gray = cv2.imread('test/will_smith.jpg', 0)
-
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-for (x, y, w, h) in faces:
-    # Draw rectangle around the face
-    cv2.rectangle(gray, (x, y), (x+w, y+h), (255, 255, 255), 3)
+
+# Crop a single face from an image.
+def faceID():
+    # Count how many faces
+    faceCount = faces.shape[0]
+    if faceCount==1:
+        x, y, w, h = faces[0]
+        crop_img = gray[y: y + h, x: x + w]
+        cv2.imwrite("test/face.jpg", crop_img)
+        return True
+    else:
+        return False
+faceID()
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
