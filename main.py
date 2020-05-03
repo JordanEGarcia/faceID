77c092ce43b28070a616f0df5e6d87f407ce3d9c
Jordan <jordaneddygarcia@gmail.com>
2020-05-03 10:18:21 -0500

    Upload now works HTML Flashes when no file is uploaded

--- main.py
+++ main.py
@@ -1,22 +1,46 @@
 import numpy
 import cv2
 import os
-from flask import Flask, flash, request, redirect, url_for, render_template
+from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory
+from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = '/img'
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+UPLOAD_FOLDER = '/home/jordan/github/faceID/img/'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 #initialize
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-#home page
+app.secret_key= "Jiodsjfoidsfjoidsfj"
+
 @app.route("/", methods = ['GET', 'POST'])
-def home():
-    if request.method=='POST':
-        return "<h1>POST</h1>"
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
     else:
         return render_template("id.html")
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
 
 if __name__ == "__main__":
     app.run(debug=True)
